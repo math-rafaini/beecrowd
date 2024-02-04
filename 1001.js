58e9c4f9d7ae9759d0710eaa88a7f6f44c279afd
Matheus Fernandes Rafaini <matheus.frafaini168@gmail.com>
2024-02-03 22:14:57 -0300

    Initial commit

---
+++ 1001.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+//console.log(lines);
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+var soma = a + b;
+
+console.log("X = " + soma);
